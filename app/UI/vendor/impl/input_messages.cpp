980d8b0dfe598a1b747e6ab6481dcfc8b764882e
blaza168 <jan.blazek10@gmail.com>
2020-10-30 14:58:09 +0100

    Create blank implementation of input_messages functions to make program runnable

--- app/UI/vendor/impl/input_messages.cpp
+++ app/UI/vendor/impl/input_messages.cpp
@@ -15,3 +15,51 @@ void display_phone_error_banner() {
     printf("Vase tel. cislo bohuzel nevyhovuje pozadavkum.\n");
     printf("Zkuste to prosim znovu: ");
 };
+
+void display_pharmacy_name_initial_banner() {
+
+}
+
+void display_pharmacy_name_error_banner() {
+
+}
+
+void display_address_city_initial_banner() {
+
+}
+
+void display_address_city_error_banner() {
+
+}
+
+void display_address_street_initial_banner() {
+
+}
+
+void display_address_street_error_banner() {
+
+}
+
+void display_postal_code_initial_banner() {
+
+}
+
+void display_postal_code_error_banner() {
+
+}
+
+void display_first_name_initial_banner() {
+
+}
+
+void display_first_name_error_banner() {
+
+}
+
+void display_last_name_initial_banner() {
+
+}
+
+void display_last_name_error_banner() {
+
+}
