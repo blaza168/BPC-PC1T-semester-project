01bc5c124a09dd95558007360e909e0995fc32ea
jd789 <jessicadrskova@gmail.com>
2020-11-04 21:28:23 +0100

    Implementation in alerts.cpp, display of pharmacy and pharmacist information

---
+++ app/UI/impl/alerts.cpp
@@ -0,0 +1,44 @@
+#include "stdio.h"
+#include "../vendor/utils/print_utils.h"
+#include "../../entity/pharmacy.h"
+#include "../../entity/pharmacist.h"
+#include "../../entity/address.h"
+
+void display_pharmacy_creation_success_alert(Pharmacy* pharmacy) {
+	printf("Creation of pharmacy was successful.\n\n");
+	printf("ID: %d\n", pharmacy->id);
+	printf("Name: %s\n", pharmacy->name);
+	printf("Phone number: %s\n", pharmacy->phone);
+	printf("Street: %s\n", pharmacy->address->street);
+	printf("City: %s\n", pharmacy->address->city);
+	printf("Postal code: %d\n", pharmacy->address->postal_code);
+	getchar();
+	clear_screen();
+}
+
+void display_pharmacy_creation_error_alert() {
+	printf("Creation of pharmacy was not successful.\n");
+	printf("This pharmacy is already registered in this system.\n\n");
+	printf("Press Enter to continue");
+	getchar();
+	clear_screen();
+}
+
+void display_pharmacist_creation_success_alert(Pharmacist* pharmacist) {
+	printf("Creation of pharmacist was successful.\n\n");
+	printf("ID: %d\n", pharmacist->id);
+	printf("Pharmacy ID: %s\n", pharmacist->pharmacy_id);
+	printf("First name: %s\n", pharmacist->first_name);
+	printf("Last name: %s\n", pharmacist->last_name);
+	printf("Phone number: %s\n", pharmacist->phone);
+	getchar();
+	clear_screen();
+}
+
+void display_pharmacist_creation_error_alert() {
+	printf("Creation of pharmacist was not successful.\n");
+	printf("This pharmacist is already registered in this system.\n\n");
+	printf("Press Enter to continue");
+	getchar();
+	clear_screen();
+}
